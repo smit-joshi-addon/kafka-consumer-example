feee75ed3e1dd321ae41b6a3fc33f61a3f2376bd
Smit Joshi <intern-j05@addontechnologies.net>
2024-09-16 17:54:22 +0530

    Kafka Consumer Example

---
+++ src/main/java/com/kafka/cunsumer/consumer/KafkaMessageListener.java
@@ -0,0 +1,35 @@
+package com.kafka.cunsumer.consumer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+import com.kafka.producer.dto.Customer;
+
+@Service
+public class KafkaMessageListener {
+
+	Logger logger = LoggerFactory.getLogger(KafkaMessageListener.class);
+
+	@KafkaListener(topics = "customTopic",groupId = "sj-consumer-group")
+	public void consume1(Customer message) {
+		logger.info("Consumer1 consume the message {}", message);
+	}
+	
+//	@KafkaListener(topics = "customTopic",groupId = "sj-consumer-group")
+//	public void consume2(String message) {
+//		logger.info("Consumer2 consume the message {}", message);
+//	}
+//	
+//	@KafkaListener(topics = "customTopic",groupId = "sj-consumer-group")
+//	public void consume3(String message) {
+//		logger.info("Consumer3 consume the message {}", message);
+//	}
+//	
+//	@KafkaListener(topics = "customTopic",groupId = "sj-consumer-group")
+//	public void consume4(String message) {
+//		logger.info("Consumer4 consume the message {}", message);
+//	}
+
+}
---
+++ src/test/java/com/kafka/cunsumer/KafkaConsumerExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kafka.cunsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaConsumerExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
